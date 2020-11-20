e2f2106971305558cd7b406c95809a40da932f8b
Matt Stine <matt.stine@gmail.com>
2020-11-20 07:39:17 -0600

    add org and org-mobile config

--- mstine-mbp/mstine-mbp.el
+++ mstine-mbp/mstine-mbp.el
@@ -3,3 +3,11 @@
 
 (setq org-default-notes-file
       "~/know/todo.org")
+
+(setq org-directory "~/know")
+
+(load "asciidoctor-mode" 'noerror)
+
+(setq org-mobile-directory "/Users/mstine/Library/Mobile Documents/iCloud~com~mobileorg~mobileorg/Documents")
+
+(setq org-mobile-inbox-for-pull "~/org-mobile-inbox/inbox.org")
